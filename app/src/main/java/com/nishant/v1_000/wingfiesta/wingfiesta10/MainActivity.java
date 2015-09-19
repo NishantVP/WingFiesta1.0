1737098ef24c8d1948f392ac404f8a6034f07f30
Nishant Phatangare <nishant1914@gmail.com>
2015-09-19 16:10:09 -0700

    Parse Quickstart Successful

--- app/src/main/java/com/nishant/v1_000/wingfiesta/wingfiesta10/MainActivity.java
+++ app/src/main/java/com/nishant/v1_000/wingfiesta/wingfiesta10/MainActivity.java
@@ -5,12 +5,24 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, MY_CONSTANTS.ParseKey_1, MY_CONSTANTS.ParseKey_2);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
     }
 
     @Override
